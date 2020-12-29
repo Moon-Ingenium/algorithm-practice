43f8cfcade3ed813610e3828e4fbaee9d288230a
Michelle Moon <robbins.michelle@ymail.com>
2020-12-28 18:50:48 -0600

    reverse letter wip

---
+++ scracth2.js
@@ -0,0 +1,8 @@
+var letterArray = ['h','e','l','l','o'];
+var leftIndex = 0;
+var rightIndex = letterArray.length-1;
+function reverseLetters(letterArray){
+    while (leftIndex< rightIndex){
+        return 
+    }
+}
