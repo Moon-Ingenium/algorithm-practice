386c95cd657893a32f8b677323eb2c8ed7545d42
Michelle Moon <robbins.michelle@ymail.com>
2021-01-02 19:38:00 -0600

    your first toy problem success

---
+++ palidrome.js
@@ -0,0 +1,18 @@
+var isPalindrome = function(x) {
+solved as string
+     var xString = "" + x;
+var reverseNumbers = xString.split("").reverse().join("");
+    if (reverseNumbers===xString){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+
+console.log(isPalindrome(-1));
+
+var xInt = Math.abs(x);
+if (Math.abs(x) === +Math.abs(x))
+}
+console.log(isPalindrome(121));
