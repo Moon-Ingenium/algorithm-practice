0d1a6cd7dabb1b574c14074c14fbcdec299ce5c6
Michelle Moon <robbins.michelle@ymail.com>
2021-01-12 11:06:18 -0600

    problem 2 practice prep

--- prep.js
+++ prep.js
@@ -1,9 +1,9 @@
-var arry= ["Andrrew", "Moon", "guy"];
-var totalChar= 0;
+var arry = ["Andrrew", "Moon", "guy"];
+var totalChar = 0;
 // combine strings in arry
 // find total sum of characters
 
-function total_characters(arry){
+function total_characters(arry) {
     // new variable as a total string
     var string = arry.join('');
     totalChar = string.length;
@@ -11,3 +11,32 @@ function total_characters(arry){
 }
 
 console.log(total_characters(arry));
+
+// var heads =0;
+// n = 10;
+// var tails = 0;
+// function flip_coin(n){
+//     // if heads increase count
+//     // if tails increase count
+//     // return number of heads
+//     // each time its flipped document if its heads or tails
+//     // 
+//     return heads; //number
+// }
+
+var arry = [304, 5]
+var sum = 0;
+var numsTotal = 0;
+var sumArry = [];
+function sum_of_numbers(arry) {
+    sum = arry[1] + arry[0];
+    sumArry.push(sum);
+    var string = arry.join('');
+    for (i = 0; i < string.length; i++) {
+        numsTotal = numsTotal + (+string[i]);
+    }
+    sumArry.push(numsTotal);
+    return sumArry;
+}
+console.log(sum_of_numbers(arry));
+//  [309, 12]
