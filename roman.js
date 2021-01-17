d5524e19bada0c3883aed38ddf40f58e75bdd1a1
Michelle Moon <robbins.michelle@ymail.com>
2021-01-17 11:49:15 -0600

    roman numerals object notation

--- roman.js
+++ roman.js
@@ -16,7 +16,7 @@ var romanToInt = function(s) {
         const cur = sym[s[i]];
         const next = sym[s[i+1]];
 
-        if (cur < next){
+        if (cur < next) {
             result += next - cur // IV -> 5 - 1 = 4
             i++
         } else {
@@ -26,3 +26,55 @@ var romanToInt = function(s) {
 
     return result; 
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var map = {
+        I: 1,
+        IV: 4,
+        V: 5,
+        IX: 9,
+        X: 10,
+        XL: 40,
+        L: 50,
+        XC: 90,
+        C: 100,
+        CD: 400,
+        D: 500,
+        CM: 900,
+        M: 1000
+    };
+    var strCopy = s.slice();
+    var key;
+    var total = 0;
+    
+    while (strCopy.length > 0) {
+        key = strCopy[0] + strCopy[1];
+        if (map[key]) { // if map.IV
+            total = total + map[key];
+            strCopy = strCopy.slice(2);
+        } else {
+            total = total + map[strCopy[0]];
+            strCopy = strCopy.slice(1);
+        }
+    }
+    return total;
+    
+};
+
+    // set object pointing str key to numeric value
+    // copy input str
+    // go through string left to right (while strCopy.length > 0)
+    // check current and next el (first and second el, str[0], str[1])
+    // if edge case subtract first number from second and slice both from string
+    // I could also first check if fisrt and second str is in the map, then just check first
+    // else slice just one character
+    // add to total and return total
+
+// III, IV, CM, 
+// convert roman numerals to integer
+// IV, XL, CD 4s edge cases
+// IX, XC, CM 9s edge case
